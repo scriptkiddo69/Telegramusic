d75f1454b3c687361b2f5bbbe2dac3df8dc935ac
scriptkiddo69 <76399109+scriptkiddo69@users.noreply.github.com>
2022-01-29 10:34:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -410,14 +410,10 @@ async def get_playlist(event: types.Message):
 @dp.message_handler(commands=['help', 'start'])
 async def help_start(event: types.Message):
     bot_info = await bot.get_me()
-    bot_name = bot_info.first_name.replace("_", "\\_").replace("*", "\\*").replace("[", "\\[").replace("`", "\\`")
-    bot_username = bot_info.username.replace("_", "\\_").replace("*", "\\*").replace("[", "\\[").replace("`", "\\`")
-    msg = "Hey, I'm *{}*\n".format(bot_name)
-    msg += "_You can use me in inline mode :_\n"
-    msg += "@{} \\(album\\|track\\|artist\\) \\<search\\>\n".format(bot_username)
-    msg += "Or just send an *Deezer* album or track *link* \\!"
+    bot_name = bot_info.first_name
+    bot_username = bot_info.username
+    msg = "Hii Bitch"
     await event.answer(msg, parse_mode="HTML")
-
 
 @dp.inline_handler()
 async def inline_echo(inline_query: InlineQuery):
